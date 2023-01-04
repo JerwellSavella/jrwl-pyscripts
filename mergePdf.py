fb582adce6e72d91644e862f4030a7a4307ae3a4
Jerwell Savella <81293539+JerwellSavella@users.noreply.github.com>
2023-01-04 15:39:45 +0800

    refactored 'mergePDF' structure

--- mergePdf.py
+++ mergePdf.py
@@ -4,33 +4,43 @@
 import os
 import PyPDF2
 
-def merge_pdfs(path, save_path):
-    pdf_files = []
-    # r=root, d=directories, f=files
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.pdf' in file:
-                pdf_files.append(os.path.join(r, file))
-    # Show files that will be merging
-    print("The following PDF files will be merged:")
-    for pdf in pdf_files:
-        file_name = os.path.basename(pdf)
-        print(file_name)
+class PDFMerger:
+    def __init__(self, path, save_path):
+        self.path = path
+        self.save_path = save_path
+        self.pdf_files = []
 
-    # Ask for confirmation before merging the files
-    response = input("Do you want to merge these files (y/n)?: ")
-    if response.lower() != 'y':
-        return
-    merger = PyPDF2.PdfFileMerger()
+    def get_pdf_files(self):
+        # r=root, d=directories, f=files
+        for r, d, f in os.walk(self.path):
+            for file in f:
+                if '.pdf' in file:
+                    self.pdf_files.append(os.path.join(r, file))
+    
+    def merge_pdfs(self):                
+        # Show files that will be merging
+        print("The following PDF files will be merged:")
+        for pdf in self.pdf_files:
+            #Extract the filename from the path
+            file_name = os.path.basename(pdf)
+            print(file_name)
 
-    for pdf in pdf_files:
-        merger.append(pdf)
+        # Ask for confirmation before merging the files
+        response = input("Do you want to merge these files (y/n)?: ")
+        if response.lower() != 'y':
+            return
+        merger = PyPDF2.PdfFileMerger()
 
-    # Specify the save path for the merged PDF file
-    merger.write(os.path.join(save_path, 'merged_pdf.pdf'))
-    merger.close()
+        for pdf in self.pdf_files:
+            merger.append(pdf)
+
+        # Specify the save path for the merged PDF file
+        merger.write(os.path.join(self.save_path, 'merged_pdf.pdf'))
+        merger.close()
 
 # Your Chosen Folders:
 path = " " # Source Folder
 save_path = " " # Save Path
-merge_pdfs(path, save_path)
+pdf_merger = PDFMerger(path, save_path)
+pdf_merger.get_pdf_files()
+pdf_merger.merge_pdfs()
